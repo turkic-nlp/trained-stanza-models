6cc64b6027b61b114b1ef78116a4e9e975788803
Sherzod Hakimov <sherzodhakimov@gmail.com>
2026-03-01 16:55:52 +0100

    release bash script

---
+++ make_release.sh
@@ -0,0 +1,8 @@
+gh release create v0.1.1 \
+  models/tk/tk_tuecl_nocharlm_tokenizer.pt \
+  models/tk/tk_tuecl_nocharlm_tagger.pt \
+  models/tk/tk_tuecl_nocharlm_lemmatizer.pt \
+  models/tk/tk_tuecl_nocharlm_parser.pt \
+  models/pretrain/tk_tuecl.pretrain.pt \
+  -t "Turkmen Stanza models v0.1.1" \
+  -n "Turkmen Stanza models and pretrain vectors."
